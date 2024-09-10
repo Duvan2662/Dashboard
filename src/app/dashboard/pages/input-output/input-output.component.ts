52743982ebf4db611b03baa923984e6ac6206990
Duvan <duvanriano2626@gmail.com>
2024-09-10 17:06:41 -0500

    Añadit productos dinamicamente

--- src/app/dashboard/pages/input-output/input-output.component.ts
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+
+import { Component, OnDestroy, signal } from '@angular/core';
 import { ProductCardComponent } from './ui/product-card/product-card.component';
+import { Product } from '@interfaces/product.interface';
+import { interval, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-input-output',
@@ -8,6 +11,37 @@ import { ProductCardComponent } from './ui/product-card/product-card.component';
   templateUrl: './input-output.component.html',
   styleUrl: './input-output.component.css'
 })
-export class InputOutputComponent {
+export class InputOutputComponent implements OnDestroy {
+  public products = signal<Product[]>([
+    {
+      id:1,
+      name:'Producto 1',
+      quantity: 0
+    },
+    {
+      id:2,
+      name:'Producto 2',
+      quantity: 0
+    }
+  ])
+
+  private intervalSubscription = interval(1000).pipe(
+    tap(() => {
+      this.products.update((products => [
+        ...products,
+        {
+          id:products.length + 1 ,
+          name:`Producto ${products.length +1}` ,
+          quantity: 0
+        }
+      ]))
+    }),
+    take(7)
+  ).subscribe();
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
+  }
+
 
 }
